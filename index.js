34a2df109d3a44907aa9cf56361102b23501c873
Naushad Karim <73757956+RfxNaushad@users.noreply.github.com>
2022-06-20 14:50:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,30 @@ const school = new School()
 
  school.startPeriod();
 
+// node server create and start 
+// // dependencies
+// const http = require('http');
+
+// // app object - module scaffolding
+// const app = {};
+
+// // configuration
+// app.config = {
+//     port: 3001
+// };
+
+// // create server
+// app.createServer = () => {
+//     const server = http.createServer(app.handleReqRes);
+//     server.listen(app.config.port, () => {
+//         console.log(`listen to the server ${app.config.port}`);
+//     });
+// };
+
+// // handle req res
+// app.handleReqRes = (req, res) => {
+//     res.end('Hello Port');
+// }
+
+// // start the server
+// app.createServer()
